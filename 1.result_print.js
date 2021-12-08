f8b7c4b6a2a103ccafea6d25fb719e0ea6e87b67
vahehak93 <vahehak@gmail.com>
2021-12-08 04:08:13 +0400

    feat: initial commit

---
+++ 1.result_print.js
@@ -0,0 +1,60 @@
+// What will be the result and why?
+
+//  + 1 + 0
+// output: 10 
+
+
+// - 1 + 0
+// output: -1 
+
+
+//  true + false
+// output: 1 
+
+
+//  6 / "3"
+// output: 2 
+
+    
+// "2" * "3"
+// output: 6 
+
+
+// 4 + 5 + "px"
+// output: 9px 
+
+
+// "$" + 4 + 5
+// output: $45 
+
+
+// "4" - 2
+// output: 2 
+
+
+// "4px" - 2
+// output: NaN
+
+
+// 7 / 0
+// output: Infinity
+
+
+//  "-9" + 5
+// output: -95
+
+
+//  "-9" - 5
+// output: -14
+
+
+// null + 1
+// output: 1
+
+
+// undefined + 1
+// output: NaN
+
+
+
+
---
+++ 2.parseInt_Float.js
@@ -0,0 +1,19 @@
+// Write logic that implements parseInt
+
+console.log(parseInt("15")) // 15
+console.log(parseInt("5.00")) // 5
+console.log(parseInt("10.3")) // 10
+console.log(parseInt("10.9")) // 10
+console.log(parseInt(" 75 ")) // 75
+console.log(parseInt("29 years")) // 29
+console.log(parseInt("I am 15 years old")) // NaN
+
+// Write logic that implements parseFloat
+
+console.log(parseFloat("15")) // 15
+console.log(parseFloat("5.00")) // 5
+console.log(parseFloat("10.3")) // 10.3
+console.log(parseFloat("10.9")) // 10.9
+console.log(parseFloat(" 75 ")) // 75
+console.log(parseFloat("29 years")) // 29
+console.log(parseFloat("I am 15 years old")) // NaN
